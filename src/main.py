25fe2b8eabbb6312f99baa8f3c980e2ff8db3a25
thibault <thibault.deverge@gmail.com>
2025-06-04 18:41:06 +0200

    Build new html

--- src/main.py
+++ src/main.py
@@ -3,17 +3,14 @@ import sys
 from src.utils.copy_utils import copy_directory
 from src.generate_page import generate_page_recursive
 
+BASEPATH = "/SSG_Static-Site-Generator/"
+
 
 def main():
-    if len(sys.argv) > 1:
-        basepath = sys.argv[1]
-    else:
-        basepath = "/"
-
     try:
         copy_directory("static", "docs")
         print("-------------------------")
-        generate_page_recursive("content/", "template.html", "docs", basepath)
+        generate_page_recursive("content/", "template.html", "docs", BASEPATH)
     except Exception as e:
         print("Error :", e)
         sys.exit(1)
