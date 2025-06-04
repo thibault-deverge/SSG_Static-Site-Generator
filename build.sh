995e42690b625ca65b0685608b66ec13582ca14d
thibault <thibault.deverge@gmail.com>
2025-06-04 18:30:29 +0200

    Add repo_name at build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$1" ]; then
-  REPO_NAME="${GITHUB_REPOSITORY##*/}"
-
-  if [ -n "$REPO_NAME" ]; then
-    BASEPATH="/${REPO_NAME}/"
-  else
-    BASEPATH="/"
-  fi
-else
-  BASEPATH="$1"
-fi
-
-python3 -m src.main "$BASEPATH"
+python3 -m src.main "/SSG_Static-Site-Generator/"
